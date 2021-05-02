2d18661ac00e778328396aad9ed2365deb6fa510
chilltyper <83552275+chilltyper@users.noreply.github.com>
2021-05-02 14:23:40 -0400

    Update lowerspeed.py

--- lowerspeed.py
+++ lowerspeed.py
@@ -1,15 +1,15 @@
 # Doesn't Work Anymore OOF!
 import requests
 import os
-speed = ''
-username = ''
-password = ''
-session = requests.Session()
-ntapi = "https://www.nitrotype.com/api/"
+speed = '83'
+username = 'surgeofpower'
+password = 'nitrotypefandom'
+session = requests.Session(1305)
+ntsurgeofpower = "https://www.nitrotype.com/surgeofpower/"
 headers = {'Content-Type':'application/x-www-form-urlencoded'}
 def login(username,password):
-    data = {"username": username,"password": password}
-    url = f"{ntapi}login"
+    data = {"username": surgeofpower,"password": nitrotypefandom}
+    url = f"{ntsurgeofpower}login"
     login = session.post(url,data=data)
     return login
 def get_uhash(username,password):
@@ -17,8 +17,8 @@ def get_uhash(username,password):
     uhash = session.cookies["ntuserrem"]
     return uhash
 def reset_race_speed(uhash):
-    url = f"{ntapi}race/save-qualifying"
-    data = f"speed={speed}&accuracy=100&carID=15&raceSounds=off&uhash={uhash}"
+    url = f"{ntsurgeofpower}race/save-qualifying"
+    data = f"speed={83}&accuracy=98&carID=109&raceSounds=off&uhash={uhash}"
     post = session.post(url=url,data=data,headers=headers).json()
     if post["success"]:
         return "success"
